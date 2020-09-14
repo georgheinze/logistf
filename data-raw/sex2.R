e0c7f465441411b2b4c3ed249df72020d0e73b89
ljiricka <lenajiricka@hotmail.com>
2020-09-14 12:54:42 +0100

    deleted raw data

--- data-raw/sex2.R
+++
@@ -1,2 +0,0 @@
-sex2 <- read.table("data-raw/sex2.txt.gz", header=TRUE)
-usethis::use_data(sex2, overwrite = TRUE)
--- data-raw/sexagg.R
+++
@@ -1,2 +0,0 @@
-sexagg <- read.table("data-raw/sexagg.txt.gz", header=T)
-usethis::use_data(sexagg, overwrite = TRUE)
