422d5d8ac61a9c1f7b1e5a3cf9759fcc37e7d9c2
Lena Jiricka <lenajiricka@hotmail.com>
2021-02-03 16:07:01 +0100

    added terms for flic/flac

--- R/terms.logistf.R
+++ R/terms.logistf.R
@@ -2,3 +2,13 @@
 terms.logistf <- function(x) {
   attr(x$model, which = 'terms', exact = TRUE)
 }
+
+#' @exportS3Method terms logistf
+terms.flic <- function(x) {
+  attr(x$model, which = 'terms', exact = TRUE)
+}
+
+#' @exportS3Method terms logistf
+terms.flac <- function(x) {
+  attr(x$model, which = 'terms', exact = TRUE)
+}
