e60ce57545357a1cfabb7991412b0030a14d80b1
Georg Heinze <georg.heinze@meduniwien.ac.at>
2021-05-14 16:04:57 +0200

    tau

--- src/logistf.c
+++ src/logistf.c
@@ -180,7 +180,7 @@ void logistffit_IRLS(double *x, int *y, int *n_l, int *k_l,
     	XtY(xt, beta_old, newresponse, k, n, 1);
     	for(i=0; i < n; i++){
     	    wi = weight[i] * pi[i] * (1.0 - pi[i]); //W^(-1)
-    	    newresponse[i] += 1/wi*((double)y[i]-pi[i] + 2 * *tau * Hdiag[i] * (1/2 - pi[i]));
+    	    newresponse[i] += 1/wi*((double)y[i]-pi[i]) + 2 * *tau * Hdiag[i] * (1/2 - pi[i]);
     	}
 
     	
