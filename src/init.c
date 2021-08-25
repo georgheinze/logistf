0021cc35e5aef218c4dae45afe60b0e4d19ee1ea
Lena Jiricka <lenajiricka@hotmail.com>
2021-08-25 14:17:17 +0200

    removed old NR algorithm

--- src/init.c
+++ src/init.c
@@ -8,7 +8,6 @@
 /* .C calls */
 extern void linpack_choleski(void *, void *);
 extern void linpack_inv_det(void *, void *, void *);
-extern void logistffit(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
 extern void logistffit_revised(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
 extern void logistffit_IRLS(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
 extern void logistplfit(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
@@ -16,7 +15,6 @@ extern void logistplfit(void *, void *, void *, void *, void *, void *, void *, 
 static const R_CMethodDef CEntries[] = {
     {"linpack_choleski", (DL_FUNC) &linpack_choleski,  2},
     {"linpack_inv_det",  (DL_FUNC) &linpack_inv_det,   3},
-    {"logistffit",       (DL_FUNC) &logistffit,       25},
     {"logistffit_revised",(DL_FUNC) &logistffit_revised,       25},
     {"logistffit_IRLS",  (DL_FUNC) &logistffit_IRLS,  24},
     {"logistplfit",      (DL_FUNC) &logistplfit,      21},
